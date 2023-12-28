d045a508b3396f39bf98dc228b616d590ec58c15
Bruno Matheus Ribeiro <brunomatheus168@gmail.com>
2023-12-28 16:10:39 -0300

    "program update"

--- src/App.java
+++ src/App.java
@@ -1,5 +1,51 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Departament;
+import entities.HourContract;
+import entities.Worker;
+import entitiesEnum.WorkerLevel;
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Enter department's name: ");
+        String departmentName = sc.nextLine();
+
+       System.out.print("Enter worker Data: ");
+       System.out.print("Name: ");
+       String workerName = sc.nextLine();
+       System.out.print("Level: ");
+       String workerLevel = sc.nextLine();
+       System.out.print("Base Salary: ");
+       double baseSalary = sc.nextDouble();
+       Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel),baseSalary, new Departament(departmentName));
+
+       System.out.print("How many contracts to this worker? ");
+       int n = sc.nextInt();
+
+       for(int i=1; i<=n; i++){
+        System.out.println("Enter contract #"+ i + " data: ");
+        System.out.print("Date (DD/MM/YYYY): ");
+        Date contracDate = sdf.parse(sc.next());
+        System.out.print("Value per Hour: ");
+        double valuePerHour = sc.nextDouble();
+        System.out.print("Duration (hours): ");
+        int hours = sc.nextInt();
+        HourContract contract = new HourContract(contracDate, valuePerHour, hours);
+        worker.addContract(contract);
+       }
+    
+
+
+
+        sc.close();
     }
 }
